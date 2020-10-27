48b60927f5bc6e6b693a9f9648336bdb595a79d5
PratibhaMastud <karandepratibha999@gmail.com>
2020-10-27 23:04:54 +0530

    Added:Calculate Daily Employee Wage

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,18 +1,30 @@
 import java.util.*;
+//Author:Pratibha Mastud
+//2.Calculate Daily Employee Wage
 
 class EmpWageComputation{
 
-public static void main(String[] args){
-int isPresent=1;
 
-if(isPresent==1){
-	System.out.println("Employee is Present");
-}else
-{
-	        System.out.println("Employee is Abcent");
-}
+	public static void main(String[] args)
+	{	
+		int wagePerHour = 20;
+		int fullDayHour = 8;
+	//method calling
+		int wageTot = calDailyWage(wagePerHour, fullDayHour);
+	//wagePerHour, fullDayHour are actual parameters
+ 
+		System.out.println("Daily Employee Wage = " + wageTot);
+	}
 
-}
+	//user defined method
 
+	//n1 ,n2 are formal parameters
+	
+	public static int calDailyWage(int n1, int n2)   
+	{	
+		int s;
+		s=n1*n2;
+		return s; //returning the sum
+	}
 
 }
